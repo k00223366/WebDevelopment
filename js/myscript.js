cb9c4a0ddb6307a83894ef7483c9111f470cf3a8
K00223366 <k00223366@my-1b20-mac-20.student.litdom.lit.ie>
2016-11-28 21:16:57 +0000

    Change text displays

--- js/myscript.js
+++ js/myscript.js
@@ -5,19 +5,46 @@ function swap(imgnum){
 		case 1:
 		document.getElementById("illustration").src = "img/DrawTokamak/PlasmaAndWallTori.png";
 		document.getElementById("illustration").title = "Plasma contained in Torus";
+        document.getElementById("changeText").innerHTML = <p>
+        Write about the first thing, the PlasmaWall tori.
+        <p>
         break;
         
         case 2:
 		document.getElementById("illustration").src = "img/DrawTokamak/ColourTorus.png";
 		document.getElementById("illustration").title = "TorusModel";
-		break;
+        document.getElementById("changeText").innerHTML =
+        <p> Write about the pink torus..<p>
+        break;
+		
 		
         case 3:
 		document.getElementById("illustration").src = "img/DrawTokamak/PlasmaWallMagnet.png";
 		document.getElementById("illustration").title = "Plasma contained in Torus";
+            document.getElementById("changeText").innerHTML =
+        <p> The Plasma and the Magnetic field <p>
         break;
 		
 		
 	}	
 	  
   }
+
+function swapText(number){
+    
+    case 1:
+    document.getElementById("changeText").innerHTML = <p>
+        Write about the first thing, the PlasmaWall tori.
+        <p>
+    break;
+          
+    case 2:
+    document.getElementById("changeText").innerHTML =
+        <p> Write about the pink torus..<p>
+    break;
+          
+    case 3:
+    document.getElementById("changeText").innerHTML =
+        <p> The Plasma and the Magnetic field <p>
+    break;
+}
