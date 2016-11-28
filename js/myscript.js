5e3f653eaa956262d4b303cbcc2ed7bc9b6f5577
K00223366 <k00223366@my-1b20-mac-20.student.litdom.lit.ie>
2016-11-28 21:26:14 +0000

    Colour of text boxes

--- js/myscript.js
+++ js/myscript.js
@@ -6,7 +6,8 @@ function swap(imgnum){
 		document.getElementById("illustration").src = "img/DrawTokamak/PlasmaAndWallTori.png";
 		document.getElementById("illustration").title = "Plasma contained in Torus";
         document.getElementById("changeText").innerHTML = 
-        "Write about the first thing, the PlasmaWall tori."
+        "Write about the first thing, the PlasmaWall tori.";
+        document.getElementById("textColumn").style.color = "orange"; 
         
         break;
         
@@ -14,7 +15,8 @@ function swap(imgnum){
 		document.getElementById("illustration").src = "img/DrawTokamak/ColourTorus.png";
 		document.getElementById("illustration").title = "TorusModel";
         document.getElementById("changeText").innerHTML =
-        "Write about the pink torus.."
+        "Write about the pink torus..";
+        document.getElementById("textColumn").style.color = "pink"; 
         break;
 		
 		
@@ -22,7 +24,8 @@ function swap(imgnum){
 		document.getElementById("illustration").src = "img/DrawTokamak/PlasmaWallMagnet.png";
 		document.getElementById("illustration").title = "Plasma contained in Torus";
         document.getElementById("changeText").innerHTML =
-        "The Plasma and the Magnetic field."
+        "The Plasma and the Magnetic field.";
+        document.getElementById("textColumn").style.color = "lightgreen"; 
         break;
 		
 		
