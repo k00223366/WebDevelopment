1fb04654cfa763fc770ecfc76945215c28baac28
K00223366 <k00223366@my-1b20-mac-20.student.litdom.lit.ie>
2016-11-28 18:26:33 +0000

    more fixing swap()

--- js/myscript.js
+++ js/myscript.js
@@ -2,14 +2,16 @@ function swap(imgnum){
 	  
 	switch(imgnum)
 	{
-		case 2:
-		document.getElementById("illustration").src = "img/DrawTokamak/ColourTorus.png";
-		document.getElementById("illustration").title = "Torus Model";
-		break;
 		case 1:
 		document.getElementById("illustration").src = "img/DrawTokamak/PlasmaAndWallTori.png";
 		document.getElementById("illustration").title = "Plasma contained in Torus";
         break;
+        
+        case 2:
+		document.getElementById("illustration").src = "img/DrawTokamak/ColourTorus.png";
+		document.getElementById("illustration").title = "TorusModel";
+		break;
+		
         case 3:
 		document.getElementById("illustration").src = "img/DrawTokamak/PlasmaWallMagnet.png";
 		document.getElementById("illustration").title = "Plasma contained in Torus";
