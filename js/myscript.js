6111f36c546b5dd638e32472d4d116880adb54e2
K00223366 <k00223366@my-1b20-mac-20.student.litdom.lit.ie>
2016-11-28 18:19:06 +0000

    fix swap(imgnum)

--- js/myscript.js
+++ js/myscript.js
@@ -3,15 +3,17 @@ function swap(imgnum){
 	switch(imgnum)
 	{
 		case 2:
-		document.getElementById("illustration").src = "img/DrawTokamak/Artboard 1.png";
+		document.getElementById("illustration").src = "img/DrawTokamak/ColourTorus.png";
 		document.getElementById("illustration").title = "Torus Model";
 		break;
 		case 1:
 		document.getElementById("illustration").src = "img/DrawTokamak/PlasmaAndWallTori.png";
 		document.getElementById("illustration").title = "Plasma contained in Torus";
-        case 1:
-		document.getElementById("illustration").src = "img/DrawTokamak/PlasmaAndWallTori.png";
+        break;
+        case 3:
+		document.getElementById("illustration").src = "img/DrawTokamak/PlasmaWallMagnet.png";
 		document.getElementById("illustration").title = "Plasma contained in Torus";
+        break;
 		
 		
 	}	
