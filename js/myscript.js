4fa89a52915e358b79eb299f38b7afc8fb1dfe53
K00223366 <k00223366@my-1b20-mac-20.student.litdom.lit.ie>
2016-11-28 21:31:15 +0000

    2 Colour of text boxes

--- js/myscript.js
+++ js/myscript.js
@@ -7,7 +7,7 @@ function swap(imgnum){
 		document.getElementById("illustration").title = "Plasma contained in Torus";
         document.getElementById("changeText").innerHTML = 
         "Write about the first thing, the PlasmaWall tori.";
-        document.getElementById("textColumn").style.color = "orange"; 
+        document.getElementById("textColumn").style.backgroundColor = "orange"; 
         
         break;
         
@@ -16,7 +16,7 @@ function swap(imgnum){
 		document.getElementById("illustration").title = "TorusModel";
         document.getElementById("changeText").innerHTML =
         "Write about the pink torus..";
-        document.getElementById("textColumn").style.color = "pink"; 
+        document.getElementById("textColumn").style.backgroundColor = "pink"; 
         break;
 		
 		
@@ -25,7 +25,7 @@ function swap(imgnum){
 		document.getElementById("illustration").title = "Plasma contained in Torus";
         document.getElementById("changeText").innerHTML =
         "The Plasma and the Magnetic field.";
-        document.getElementById("textColumn").style.color = "lightgreen"; 
+        document.getElementById("textColumn").style.backgroundColor = "lightgreen"; 
         break;
 		
 		
