d0411729949e5a634696462bbf1008de09d241ba
K00223366 <k00223366@my-1b20-mac-20.student.litdom.lit.ie>
2016-11-28 10:28:09 +0000

    almost initial commit

---
+++ js/myscript.js
@@ -0,0 +1,19 @@
+function swap(imgnum){
+	  
+	switch(imgnum)
+	{
+		case 2:
+		document.getElementById("illustration").src = "img/DrawTokamak/Artboard 1.png";
+		document.getElementById("illustration").title = "Torus Model";
+		break;
+		case 1:
+		document.getElementById("illustration").src = "img/DrawTokamak/PlasmaAndWallTori.png";
+		document.getElementById("illustration").title = "Plasma contained in Torus";
+        case 1:
+		document.getElementById("illustration").src = "img/DrawTokamak/PlasmaAndWallTori.png";
+		document.getElementById("illustration").title = "Plasma contained in Torus";
+		
+		
+	}	
+	  
+  }
