d8561ca98a7571dfee3d96f77b048e83000c577d
David Wright <davidjohnwright@davids-imac.zyxel.com>
2016-12-04 18:42:12 +0000

    Use child nodes to fix paragraphs colours

--- js/myscript.js
+++ js/myscript.js
@@ -8,9 +8,16 @@ function swap(imgnum){
         document.getElementById("textColumn").innerHTML=
         "<h1>A Blanket</h1>";
         document.getElementById("changeText").innerHTML = 
-        "<p>The inside of the torus is lined with a blanket or thermal shield, marked orange. It has two jobs: <p>To shield the walls of the tokamak from the heat </p> <p> To trap the neutrons flying off the plasma.</p> <p>As they slam into the blanket it will heat up. The heat will boil water. Steam will drive turbines and make electricity.</p><p></p>";
+        "<p class=text>The inside of the torus is lined with a blanket or thermal shield, marked orange. It has two jobs: <p class=text>To shield the walls of the tokamak from the heat </p> <p class=text> To trap the neutrons flying off the plasma.</p> <p class=text>As they slam into the blanket it will heat up. The heat will boil water. Steam will drive turbines and make electricity.</p><p></p>";
         document.getElementById("textColumn").style.backgroundColor = "orange"; 
-        document.getElementById("changeText").style.backgroundColor = "orange";   
+      
+        var x = document.querySelectorAll("p.text");
+        var i;
+        for (i=0;i<x.length;i++)
+            {
+                x[i].style.backgroundColor = "orange";
+            }
+            /*document.getElementById("changeText").style.backgroundColor = "orange"; */  
         
         break;
         
@@ -20,9 +27,16 @@ function swap(imgnum){
         document.getElementById("textColumn").innerHTML =
         "<h1>The Torus is just like an inner tube.</h1>"    
         document.getElementById("changeText").innerHTML =
-        "<p>The torus chamber does not trap the superheated gas when it turns into plasma.</p><p> It traps the heat of the plasma that is used to power turbines.</p><p>No material can withstand the heat of the plasma.</p><p> It is hotter than the surface of the sun.</p><p>Tokamak is a Russian word</p> <p>Made up two parts</p><p>Torus and</p><p>Magnets, they are the key. They stop the tokamak from being destroyed.</p>";
+        "<p class=text>The torus chamber cannot trap the plasma.</p><p class=text> It can only capture the heat of the plasma.</p><p class=text>The plasma would destroy anything it touches.</p><p class=text> It is hotter than the surface of the sun.</p><p class=text>Tokamak is a Russian word made up two parts</p><p class=text>Torus and</p><p class=text>Magnets, they are the key. They stop the tokamak from being destroyed.</p>";
         document.getElementById("textColumn").style.backgroundColor = "pink"; 
-        document.getElementById("changeText").style.backgroundColor = "pink"; 
+    /*document.getElementById("changeText").style.backgroundColor = "pink"; */
+      
+        var x = document.querySelectorAll("p.text");
+        var i;
+        for (i=0;i<x.length;i++)
+            {
+                x[i].style.backgroundColor = "pink";
+            }    
         break;
 		
 		
@@ -30,11 +44,21 @@ function swap(imgnum){
 		document.getElementById("illustration").src = "img/DrawTokamak/PlasmaWallMagnet.png";
 		document.getElementById("illustration").title = "Plasma contained in Torus";
         document.getElementById("textColumn").innerHTML =
-        "<h1>The Magnetic Bottle.</h1>";
+        "<h1>The Genie in the Bottle.</h1>";
         document.getElementById("changeText").innerHTML =
-        "<p>A very powerful magnet is needed to contain the power of the sun</p><p>This one is 17 meters by 9 meters.</p><p>It weights 310 tonnes.</p><p>The torus will sit inside 18 of them.</p><p>Plasma makes stars: it is similar to a gas but much much lighter.<p>This plasma will exist at 150 million degrees  centigrad.</p></p><p>Electrons are ripped from their nuclei.</p><p>They are negatively charged and can be trapped by the powerful magnetic field wrapping the torus</p><p>The magnets are the only thing stopping the plasma from destroying the torus.</p><p>And they will work just fine operating at -269 degrees centigrade. Absolute zero is thought to be -273 degrees.";
+        "<p class=text>A very powerful magnet is needed to contain the power of the sun</p><p class=text>This one is 17 meters by 9 meters.</p><p class=text>It weights 310 tonnes.</p><p class=text>The torus will sit inside 18 of them.</p><p class=text>Plasma makes stars: it is similar to a gas but much much lighter.<p class=text>This plasma will exist at 150 million degrees  centigrad.</p><p class=text>Electrons are ripped from their nuclei.</p><p class=text>They are negatively charged and can be trapped by the powerful magnetic field wrapping the torus</p><p class=text>The magnets are the only thing stopping the plasma from destroying the torus.</p><p class=text>And they will work just fine operating at -269 degrees centigrade. Absolute zero is thought to be -273 degrees.</p>";
         document.getElementById("textColumn").style.backgroundColor = "lightgreen";
-        document.getElementById("changeText").style.backgroundColor = "lightgreen";
+        /*document.getElementById("changeText").style.backgroundColor = "lightgreen";*/
+            
+        var x = document.querySelectorAll("p.text");
+        var i;
+        for (i=0;i<x.length;i++)
+            {
+                x[i].style.backgroundColor = "lightgreen";
+            }    
+        break;    
+            
+            
         break;
 		
 		
